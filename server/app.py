8fe51ced50a6095c95c01c30611a031948e6ebe6
jlapthorne <jlapthorne@student.unimelb.edu.au>
2017-09-08 21:48:15 +1000

    Cross-platform support
    This one weird trick makes your code run on Windows

--- server/app.py
+++ server/app.py
@@ -53,5 +53,5 @@ app.register_blueprint(api_bp)
 if __name__ == "__main__":
     from gevent import pywsgi
     from geventwebsocket.handler import WebSocketHandler
-    server = pywsgi.WSGIServer(('', 5000), app, handler_class=WebSocketHandler)
+    server = pywsgi.WSGIServer(('0.0.0.0', 5000), app, handler_class=WebSocketHandler)
     server.serve_forever()
