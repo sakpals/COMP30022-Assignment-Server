c5de02028400fdb7e6020f1c800414bac1f20556
MDunwoodie <mdunwoodie@student.unimelb.edu.au>
2017-10-14 09:53:11 +1100

    MOD use PIL and python-image-resize to resize image.
    There was issues with LibMagick segfaulting, so hopefully this fixes it.

--- server/resources/image.py
+++ server/resources/image.py
@@ -1,7 +1,8 @@
 from flask import request, send_from_directory, current_app
 from flask_restful import Resource, reqparse, fields, marshal_with
 from werkzeug.datastructures import FileStorage
-from pgmagick import Image, Blob
+from resizeimage import resizeimage
+from PIL import Image
 
 from common.auth import authenticate
 from common.randomstring import generate as gen_rs
@@ -20,10 +21,10 @@ class ImageUpload(Resource):
 
         filename ='%s.jpg' % gen_rs(16)
         try:
-            im = Image(Blob(args['image'].read()))
-            im.quality(80)
-            im.scale('800x600')
-            im.write(current_app.config['IMAGE_UPLOAD_DIRECTORY'] + filename)
+            with open(args['image'], 'r+b') as f:
+                with Image.open(f) as image:
+                    img = resizeimage.resize_cover(image, [400, 400])
+                    img.save(current_app.config['IMAGE_UPLOAD_DIRECTORY'] + filename, 'JPEG')
         except:
             raise TypeError("Invalid Image")
 
