2eca2815c2547d2eceb5b5881e099cf789eb3ee3
MDunwoodie <mdunwoodie@student.unimelb.edu.au>
2017-10-22 23:44:05 +1100

    ADD message to run-tests.sh, and duplicate user test

--- run-tests.sh
+++ run-tests.sh
@@ -4,12 +4,13 @@ TEST_URL=http://127.0.0.1:5000
 
 curl $TEST_URL -s > /dev/null
 if [[ $? != 0 ]] ; then
-  echo "Unable to connect to: $TEST_URL"
-  echo "Are you sure the server is running?"
-  exit $rc
+  echo "Server is not running."
+  exit -1
 fi
+
+echo "Please run tests with a fresh/clean database (rm server/tmp.db)"
+echo "Otherwise tests will fail"
 
 for x in tests/* ; do
   pyresttest $TEST_URL $x
 done
-
